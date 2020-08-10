f188df0f634d4067b5d3de9e270ddc5eb7b75ee4
hfhbd <mybztg@icloud.com>
2020-08-10 10:53:22 +0200

    move serialization plugin version from settings to hardcoded build.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,2 @@
 include(":app")
 rootProject.name = "My ApplicationResult"
-
-pluginManagement {
-    val kotlinVersion: String by settings
-    plugins {
-        kotlin("plugin.serialization") version kotlinVersion
-    }
-}
