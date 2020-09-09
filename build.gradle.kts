03029603a3162eb71c9f8bbf5b9f31bd9d2c8369
hfhbd <mybztg@icloud.com>
2020-09-09 19:38:32 +0200

    update to jetpack compose alpha02

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,6 @@ buildscript {
     dependencies {
         val kotlinVersion: String by project
         classpath("com.android.tools.build", "gradle", "4.0.1")
-        classpath(kotlin("gradle-plugin:$kotlinVersion"))
+        classpath(kotlin("gradle-plugin", kotlinVersion))
     }
 }
