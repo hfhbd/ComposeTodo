dec8df8e330abd46559b12eb9f4a8880ac3cf677
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2021-02-04 17:06:07 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     // Apache 2, https://github.com/JetBrains/kotlin/releases/latest
-    kotlin("multiplatform") version "1.4.21" apply false
+    kotlin("multiplatform") version "1.4.21-2" apply false
     kotlin("plugin.serialization") version "1.4.30" apply false
     id("com.android.application") version "7.0.0-alpha05" apply false
 }
@@ -12,10 +12,9 @@ repositories {
 
 allprojects {
     repositories {
-        jcenter()
         mavenCentral()
         google()
         maven("https://dl.bintray.com/cy6ergn0m/uuid")
-        maven(url = "https://kotlin.bintray.com/kotlinx/") // kotlinx.datetime
+        maven(url = "https://kotlin.bintray.com/kotlinx/")
     }
 }
