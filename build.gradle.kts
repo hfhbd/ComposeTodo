a51bc85521ad72c9bca880f9ee3992c9432b880f
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2021-02-04 15:08:57 +0100

    Refactor for Dependabot

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    val kotlinVersion = "1.4.21" // Apache 2, https://github.com/JetBrains/kotlin/releases/latest
-    kotlin("multiplatform") version kotlinVersion apply false
-    kotlin("plugin.serialization") version kotlinVersion apply false
+    // Apache 2, https://github.com/JetBrains/kotlin/releases/latest
+    kotlin("multiplatform") version "1.4.21" apply false
+    kotlin("plugin.serialization") version "1.4.21" apply false
     id("com.android.application") version "7.0.0-alpha05" apply false
 }
 
