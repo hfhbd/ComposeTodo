9ebf807e27533eb36df653aaf317fec93a259b78
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2021-03-05 00:07:57 +0100

    Remove suppression (#81)
    Co-authored-by: hfhbd <hfhbd@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,6 @@ allprojects {
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
 
         maven(url = "https://kotlin.bintray.com/kotlinx/") // https://github.com/Kotlin/kotlinx-datetime/issues/40
-        @Suppress("JcenterRepositoryObsolete")
         jcenter() // https://github.com/JetBrains/Exposed/issues/1160
     }
 }
