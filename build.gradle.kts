338965e0a12f26c7e6c8f2cad5919da08a0ca587
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2021-04-02 13:50:05 +0200

    Remove JCenter (#116)

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,8 +19,5 @@ allprojects {
         maven(url = "https://maven.pkg.jetbrains.space/public/p/compose/dev")
         maven(url = "https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-js-wrappers")
         maven(url = "https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven")
-
-        maven(url = "https://kotlin.bintray.com/kotlinx/") // https://github.com/Kotlin/kotlinx-datetime/issues/40
-        jcenter() // https://github.com/JetBrains/Exposed/issues/1160
     }
 }
