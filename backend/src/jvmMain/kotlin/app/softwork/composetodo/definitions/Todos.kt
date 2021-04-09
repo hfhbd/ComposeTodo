d88f8038287246667a2f6261d3d0678d9dcd3d8f
hfhbd <hfhbd@users.noreply.github.com>
2021-04-09 13:35:22 +0200

    Make the until date optional

--- backend/src/jvmMain/kotlin/app/softwork/composetodo/definitions/Todos.kt
+++ backend/src/jvmMain/kotlin/app/softwork/composetodo/definitions/Todos.kt
@@ -6,6 +6,6 @@ import org.jetbrains.exposed.sql.`java-time`.datetime
 object Todos: KotlinxUUIDTable() {
     val user = reference("userID", Users)
     val title = varchar("title", 512)
-    val until = datetime("until")
+    val until = datetime("until").nullable()
     val finished = bool("finished")
 }
