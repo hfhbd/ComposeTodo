3a3748f493aa8821d2f059df76b8d89b17729018
hfhbd <hfhbd@users.noreply.github.com>
2021-04-09 20:39:52 +0200

    Add RateLimit

--- backend/src/jvmMain/kotlin/app/softwork/composetodo/main.kt
+++ backend/src/jvmMain/kotlin/app/softwork/composetodo/main.kt
@@ -1,12 +1,16 @@
 package app.softwork.composetodo
 
+import io.ktor.application.*
 import io.ktor.server.cio.*
 import io.ktor.server.engine.*
-import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.*
+import kotlin.time.*
 
+@ExperimentalTime
 fun main() {
     Database.connect("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;", "org.h2.Driver")
     embeddedServer(CIO) {
+        install(RateLimit)
         TodoModule()
     }.start(wait = true)
 }
