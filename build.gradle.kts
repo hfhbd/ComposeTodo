221897ccda30d2306fb7969e1b0303dbd9bf717f
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2021-05-25 22:46:01 +0200

    Bump compose to 211 (#230)
    * Bump compose to 211
    
    * Bump Kotlin to 1.5.10
    
    Co-authored-by: hfhbd <hfhbd@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
     // Apache 2, https://github.com/JetBrains/kotlin/releases/latest
-    kotlin("multiplatform") version "1.5.0" apply false
-    kotlin("plugin.serialization") version "1.5.0" apply false
+    kotlin("multiplatform") version "1.5.10" apply false
+    kotlin("plugin.serialization") version "1.5.10" apply false
     id("com.android.application") version "7.0.0-beta02" apply false
-    id("org.jetbrains.compose") version "0.4.0-build210" apply false
+    id("org.jetbrains.compose") version "0.4.0-build211" apply false
 }
 
 repositories {
