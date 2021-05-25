dca956925c9b392c21e0b56c22a15c5760bde6d8
hfhbd <hfhbd@users.noreply.github.com>
2021-05-25 22:37:24 +0200

    Bump Kotlin to 1.5.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     // Apache 2, https://github.com/JetBrains/kotlin/releases/latest
-    kotlin("multiplatform") version "1.5.0" apply false
-    kotlin("plugin.serialization") version "1.5.0" apply false
+    kotlin("multiplatform") version "1.5.10" apply false
+    kotlin("plugin.serialization") version "1.5.10" apply false
     id("com.android.application") version "7.0.0-beta02" apply false
     id("org.jetbrains.compose") version "0.4.0-build211" apply false
 }
