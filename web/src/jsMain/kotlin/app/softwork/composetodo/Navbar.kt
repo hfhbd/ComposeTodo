be85dd8f3388fdd1cee7759d211efacb1afcd759
hfhbd <hfhbd@users.noreply.github.com>
2021-06-02 23:15:43 +0200

    Fix navbar layout

--- web/src/jsMain/kotlin/app/softwork/composetodo/Navbar.kt
+++ web/src/jsMain/kotlin/app/softwork/composetodo/Navbar.kt
@@ -9,42 +9,49 @@ import org.jetbrains.compose.web.dom.*
 @Composable
 fun Navbar(api: API, onLogout: () -> Unit) {
     Nav(attrs = {
-        classes("navbar", "navbar-expand-${Breakpoint.Medium}", "navbar-${Color.Dark}", "sticky-top", "bg-${Color.Dark}")
+        classes(
+            "navbar",
+            "navbar-expand-${Breakpoint.Medium}",
+            "navbar-${Color.Dark}",
+            "sticky-top",
+            "bg-${Color.Dark}"
+        )
     }) {
-        Container(Breakpoint.ExtraExtraLarge) {
+        Container(fluid = true, Breakpoint.ExtraExtraLarge) {
             Brand {
                 Text("Softwork.app")
             }
-        }
-        if (api is API.LoggedIn) {
 
-            Toggler("navbarNav", "navbarNav")
-            Div(attrs = {
-                classes("collapse", "navbar-collapse")
-                id("navbarNav")
-            }) {
-                Ul(attrs = { classes("navbar-nav", "mr-auto") }) {
-                    Li(attrs = { classes("nav-item") }) {
-                        NavLink(attrs = { classes("nav-link") }, to = "/") {
-                            Text("Tours")
+            if (api is API.LoggedIn) {
+
+                Toggler("navbarNav", "navbarNav")
+                Div(attrs = {
+                    classes("collapse", "navbar-collapse")
+                    id("navbarNav")
+                }) {
+                    Ul(attrs = { classes("navbar-nav", "me-auto") }) {
+                        Li(attrs = { classes("nav-item") }) {
+                            NavLink(attrs = { classes("nav-link") }, to = "/") {
+                                Text("Tours")
+                            }
+                        }
+                        Li(attrs = { classes("nav-item") }) {
+                            NavLink(attrs = { classes("nav-link") }, to = "/users") {
+                                Text("Users")
+                            }
                         }
                     }
-                    Li(attrs = { classes("nav-item") }) {
-                        NavLink(attrs = { classes("nav-link") }, to = "/users") {
-                            Text("Users")
+                    A(attrs = {
+                        classes("btn", "btn-outline-${Color.Light}")
+                        onClick {
+                            scope.launch {
+                                api.logout()
+                                onLogout()
+                            }
                         }
+                    }, href = "/#") {
+                        Text("Logout")
                     }
-                }
-                A(attrs = {
-                    classes("btn", "btn-outline-${Color.Light}")
-                    onClick {
-                        scope.launch {
-                            api.logout()
-                            onLogout()
-                        }
-                    }
-                }, href = "/#") {
-                    Text("Logout")
                 }
             }
         }
