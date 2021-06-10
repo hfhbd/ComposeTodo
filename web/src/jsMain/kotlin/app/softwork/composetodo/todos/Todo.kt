3591964beecb8e7b33ca6fe64de0462dac009a08
hfhbd <hfhbd@users.noreply.github.com>
2021-06-10 02:25:31 +0200

    Use working routing

---
+++ web/src/jsMain/kotlin/app/softwork/composetodo/todos/Todo.kt
@@ -0,0 +1,21 @@
+package app.softwork.composetodo.todos
+
+import androidx.compose.runtime.*
+import app.softwork.composetodo.*
+import app.softwork.composetodo.dto.*
+import kotlinx.coroutines.*
+import kotlinx.uuid.*
+import org.jetbrains.compose.web.dom.*
+
+@Composable
+fun Todo(api: API.LoggedIn, todoID: UUID) {
+    var todo: Todo? by remember { mutableStateOf(null) }
+    LaunchedEffect(todoID) {
+        scope.launch {
+            todo = api.getTodo(todoID)
+        }
+    }
+    todo?.let {
+        Text(it.title)
+    }
+}
