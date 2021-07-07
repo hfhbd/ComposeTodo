62d63d6c3578e0261b10d3d782f872be30274991
hfhbd <hfhbd@users.noreply.github.com>
2021-07-07 11:19:58 +0200

    Remove macOS

--- iosApp/Shared/composetodoApp.swift
+++ iosApp/Shared/composetodoApp.swift
@@ -13,8 +13,9 @@ struct ComposeTodoApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            NavigationView {
+                ContentView().environment(\.managedObjectContext, persistenceController.container.viewContext)
+            }
         }
     }
 }
