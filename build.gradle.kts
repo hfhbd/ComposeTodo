4bc3b64e92873f3c9c1fd875b8ef5da824516956
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2021-07-23 23:27:23 +0200

    Bump to Build262 (#315)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
     // Apache 2, https://github.com/JetBrains/kotlin/releases/latest
-    kotlin("multiplatform") version "1.5.10" apply false
+    kotlin("multiplatform") version "1.5.21" apply false
     kotlin("plugin.serialization") version "1.5.10" apply false
     id("com.android.application") version "4.2.2" apply false
-    id("org.jetbrains.compose") version "0.5.0-build245" apply false
+    id("org.jetbrains.compose") version "0.5.0-build262" apply false
 }
 
 repositories {
@@ -17,12 +17,5 @@ allprojects {
         google()
 
         maven(url = "https://maven.pkg.jetbrains.space/public/p/compose/dev")
-
-        maven(url = "https://maven.pkg.github.com/hfhbd/*") {
-            credentials {
-                username = System.getProperty("gpr.user") ?: System.getenv("GITHUB_ACTOR")
-                password = System.getProperty("gpr.key") ?: System.getenv("GITHUB_TOKEN")
-            }
-        }
     }
 }
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -14,13 +14,13 @@ kotlin {
         commonMain {
             dependencies {
                 // Apache 2, https://github.com/Kotlin/kotlinx.serialization/releases/latest
-                api("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.1")
+                api("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.2")
 
                 // Apache 2, https://github.com/Kotlin/kotlinx-datetime/releases
                 api("org.jetbrains.kotlinx:kotlinx-datetime:0.2.1")
 
                 // Apache 2, https://github.com/hfhbd/kotlinx-uuid/releases
-                api("app.softwork:kotlinx-uuid-core:0.0.6")
+                api("app.softwork:kotlinx-uuid-core:0.0.9")
 
                 // Apache 2, https://github.com/ktorio/ktor/releases/latest
                 api("io.ktor:ktor-client-core:1.6.1")
@@ -29,6 +29,15 @@ kotlin {
         commonTest {
             dependencies {
                 implementation(kotlin("test"))
+            }
+        }
+        val iosArm64Main by getting {
+            dependencies {
+                // Apache 2, https://github.com/ktorio/ktor/releases/latest
+                api("io.ktor:ktor-client-ios:1.6.1")
+
+                // Apache 2, https://github.com/Kotlin/kotlinx.coroutines/releases/latest
+                api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1-native-mt")
             }
         }
     }
