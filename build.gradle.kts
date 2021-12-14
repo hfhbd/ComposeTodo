bc8a6772f0170250a991dd26696794ba1d7b5704
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2021-12-14 15:20:11 +0100

    Use Compose 1.0.1-rc2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
     // Apache 2, https://github.com/JetBrains/kotlin/releases/latest
-    kotlin("multiplatform") version "1.6.10-RC" apply false
+    kotlin("multiplatform") version "1.6.10" apply false
     kotlin("plugin.serialization") version "1.6.10" apply false
     id("com.android.application") version "7.0.4" apply false
-    id("org.jetbrains.compose") version "1.0.1-rc1" apply false
+    id("org.jetbrains.compose") version "1.0.1-rc2" apply false
     id("com.squareup.sqldelight") version "1.5.3" apply false
 }
 
