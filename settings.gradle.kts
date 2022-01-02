254154c724d48a4a6b5329feb746848cec6b9a91
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2022-01-02 15:12:49 +0100

    Remove outdated snapshot repos

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,10 +3,6 @@ pluginManagement {
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
         google()
         gradlePluginPortal()
-        mavenCentral()
-        maven("https://www.jetbrains.com/intellij-repository/releases")
-        maven("https://jetbrains.bintray.com/intellij-third-party-dependencies")
-        maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
     }
 }
 
