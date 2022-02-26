32fc1b974f01c33555804962ad1c6e0b0e6c6e6f
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2022-02-26 18:45:56 +0100

    Install brew manually

--- iosApp/ci_scripts/ci_post_clone.sh
+++ iosApp/ci_scripts/ci_post_clone.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install openjdk
 /Users/local/Homebrew/opt/openjdk/bin/java --version
