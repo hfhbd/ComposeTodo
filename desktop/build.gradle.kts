8e2f412f9229353a50138c7604cb82f2c757b04f
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2022-04-29 15:25:25 +0200

    Use ktor variable in dependency (#507)
    Co-authored-by: hfhbd <hfhbd@users.noreply.github.com>

--- desktop/build.gradle.kts
+++ desktop/build.gradle.kts
@@ -7,7 +7,8 @@ dependencies {
     implementation(projects.composeClients)
     testImplementation(kotlin("test"))
 
-    implementation("io.ktor:ktor-client-cio:2.0.0")
+    val ktor = "2.0.0"
+    implementation("io.ktor:ktor-client-cio:$ktor")
     implementation("com.squareup.sqldelight:sqlite-driver:1.5.3")
     implementation(compose.desktop.currentOs)
 }
