25bbdbccfb9bee1123da637ed9efdaa7994f9024
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2022-12-28 01:03:21 +0100

    Refactor android build logic (#679)
    Co-authored-by: hfhbd <hfhbd@users.noreply.github.com>

---
+++ build-logic/src/main/kotlin/androidConfigs.kt
@@ -0,0 +1,26 @@
+import com.android.build.api.dsl.*
+import org.gradle.api.*
+
+@Suppress("MagicNumber")
+fun <BuildFeaturesT : BuildFeatures,
+    BuildTypeT : BuildType,
+    DefaultConfigT : DefaultConfig,
+    ProductFlavorT : ProductFlavor> CommonExtension<
+    BuildFeaturesT,
+    BuildTypeT,
+    DefaultConfigT,
+    ProductFlavorT
+    >.androidConfig() {
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 26
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+const val TARGET_SDK = 33
