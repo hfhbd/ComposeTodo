57eb2e0a94f1bfd48dcf454062e96219262df9cd
hfhbd <hfhbd@users.noreply.github.com>
2023-08-01 15:36:47 +0200

    Disable signature clasher

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -30,3 +30,11 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 // enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
 
 include(":shared")
+
+include(":backend")
+
+include(":clients")
+include(":composeClients")
+include(":web")
+include(":androidApp")
+include(":desktop")
