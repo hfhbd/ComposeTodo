dea07aed6575bbb88ae7e64347020347acfd0d8f
hfhbd <hfhbd@users.noreply.github.com>
2024-03-26 14:00:00 +0100

    Bump Kotlin to 2.0.0-Beta5

--- composeClients/build.gradle.kts
+++ composeClients/build.gradle.kts
@@ -16,6 +16,7 @@ kotlin {
                 api(projects.clients)
                 api(compose.foundation)
                 api(compose.material)
+                api(compose.preview)
             }
         }
 
