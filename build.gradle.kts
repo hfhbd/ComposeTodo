072dca8c86384153cf0eb0515a308da3ed01228c
hfhbd <22521688+hfhbd@users.noreply.github.com>
2024-09-09 11:47:26 +0200

    Add nodejs repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,3 +34,7 @@ tasks {
         }
     }
 }
+
+plugins.withType<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin> {
+    the<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension>().downloadBaseUrl = null
+}
