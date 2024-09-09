093a7f7ad7257eb2464d7495944df2ce30238e8d
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2024-09-09 12:04:23 +0200

    Add nodejs repo (#1025)

--- gradle/build-logic/src/main/kotlin/MyRepos.settings.gradle.kts
+++ gradle/build-logic/src/main/kotlin/MyRepos.settings.gradle.kts
@@ -1,6 +1,18 @@
 dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         mavenCentral()
         google()
+        exclusiveContent {
+            forRepository {
+                ivy("https://nodejs.org/dist/") {
+                    name = "Node Distributions at $url"
+                    patternLayout { artifact("v[revision]/[artifact](-v[revision]-[classifier]).[ext]") }
+                    metadataSources { artifact() }
+                    content { includeModule("org.nodejs", "node") }
+                }
+            }
+            filter { includeGroup("org.nodejs") }
+        }
     }
 }
