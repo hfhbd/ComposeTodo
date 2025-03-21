b7694f591be61ab30eadf44a24469f6e4511abe0
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2025-03-21 15:16:20 +0100

    Add build-health

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,6 +9,7 @@ pluginManagement {
 
 plugins {
     id("MyRepos")
+    id("com.autonomousapps.build-health") version "2.13.0"
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
     id("com.gradle.develocity") version "3.19.1"
 }
